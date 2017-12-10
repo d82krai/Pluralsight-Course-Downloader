2a5ce288d3098ff52a33095584a07841b5eed8b7
Deepak Rai <deep.k.rai@gmail.com>
2017-12-10 09:40:58 +0530

    No need to press "s" button, auto-download support
    First of all I thank you Vatsal Joshi. I got something to start with.
    Here are the changes I made:
    1) No need press "s" button. As soon as video ends it will automatically start downloading
    2) After every module completes Pluralsight open a popup. Which says "Do you want to continue with next module?". This script automatically clicking "Continue" button.
    
    Overall you don't need to worry anything. This script will handle download of complete course.
    
    ---
    Disclaimer: I don't encourage download video from  pluralsight.

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-$(function () {
+$(function() {
 
-  $(document).keydown(function (e) {
+    setTimeout(function() {
+        document.getElementById('vjs_video_3_html5_api').addEventListener('ended', myHandler, false);
+        function myHandler(e) {
+            var link = $("#vjs_video_3_html5_api").attr("src");
+            var coursename = $("#course-title-link").text();
+            coursename = coursename.replace(/[\/:?><]/g, "");
+            var folder = $("li.selected").parents("ul").prev("header").children("div").eq(1);
+            folder = (folder.parents("section.module.open").eq(0).index() + 1) + " - " + folder.find("h2").text();
+            folder = folder.replace(/[\/:?><]/g, "");
 
-    if (e.key === "s") {
+            var filename = $("li.selected").eq(1).index() + 1 + " - " + $("#module-clip-title").text().split(" : ").pop().trim() + "." + link.split("?")[0].split(".").pop();
+            filename = filename.replace(/[\/:?><]/g, "");
 
-      var link = $("#vjs_video_3_html5_api").attr("src");
+            chrome.runtime.sendMessage({
+                greeting: "download",
+                link: link,
+                filename: "Pluralsight/" + coursename + "/" + folder + "/" + filename
+            }, function(response) {
+                console.log(response.farewell);
+            });
 
-      var coursename = $("#course-title-link").text();
-      coursename = coursename.replace(/[\/:?><]/g, "");
-
-      var folder = $("li.selected").parents("ul").prev("header").children("div").eq(1);
-      folder = (folder.parents("section.module.open").eq(0).index() + 1) + " - " + folder.find("h2").text();
-      folder = folder.replace(/[\/:?><]/g, "");
-
-      var filename = $("li.selected").eq(1).index() + 1 + " - " + $("#module-clip-title").text().split(" : ").pop().trim() + "." + link.split("?")[0].split(".").pop();
-      filename = filename.replace(/[\/:?><]/g, "");
-
-      chrome.runtime.sendMessage({
-        greeting: "download",
-        link: link,
-        filename: "Pluralsight/" + coursename + "/" + folder + "/" + filename
-      }, function (response) {
-        console.log(response.farewell);
-      });
-
-    }
-
-  });
-
+            setTimeout(function() {
+                var btnNextModule = $('#next-module');
+                if (btnNextModule != null) {
+                    var isVisible = $(btnNextModule).is(':visible');
+                    if (isVisible) {
+                        $(btnNextModule).click();
+                    }
+                }
+            }, 7000)
+        }
+    }, 7000);
 });
